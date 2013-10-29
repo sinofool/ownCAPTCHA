f651a7f00c5cec44b1d5a2e1d52071e2cd437ba4
Bochun Bai <bbc@sinofool.net>
2013-10-29 16:16:26 +0800

    ImageMagick compiled

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
 
 CPP=g++
-LIBGD_HOME=$(HOME)/code/build/libgd
+IMAGEMAGICK_HOME=$(HOME)/code/build/imagemagick
+IMAGEMAGICK_INCLUDE=$(IMAGEMAGICK_HOME)/include/ImageMagick-6
+IMAGEMAGICK_LIB=$(IMAGEMAGICK_HOME)/lib
 
-CXXFLAGS=-I$(LIBGD_HOME)/include
-LDFLAGS=-L$(LIBGD_HOME)/lib -lgd
+LIBPNG_HOME=$(HOME)/code/build/libpng
+LIBPNG_LIB=$(LIBPNG_HOME)/lib
+
+JPEG_HOME=$(HOME)/code/build/jpeg-9
+JPEG_LIB=$(JPEG_HOME)/lib
+
+CXXFLAGS=-I$(IMAGEMAGICK_INCLUDE) -DMAGICKCORE_HDRI_ENABLE=0 -DMAGICKCORE_QUANTUM_DEPTH=16
+LDFLAGS=-L$(IMAGEMAGICK_LIB) -lMagick++-6.Q16 -lMagickWand-6.Q16 -lMagickCore-6.Q16 -lz -lxml2 \
+ -L$(LIBPNG_LIB) -lpng -L$(JPEG_LIB) -ljpeg -lbz2
 
 .PHONY: all
 .PHONY: clean
@@ -21,5 +30,5 @@ run:
 *.o: *.cpp
 	$(CPP) -c -g -o $@ $(CXXFLAGS) $<
 
-ownCAPTCHA: src/main/cpp/main.cpp
-	$(CPP) -g -o $@ $(CXXFLAGS) $(LDFLAGS) $^
+ownCAPTCHA: src/main/cpp/main.cpp src/main/cpp/captcha.hpp
+	$(CPP) -g -o $@ $(CXXFLAGS) $(LDFLAGS) $<
--- src/main/cpp/captcha.hpp
+++ src/main/cpp/captcha.hpp
@@ -1,88 +1,104 @@
-#include <gd.h>
+#include <Magick++.h>
 #include <string>
 #include <stdio.h>
 
 struct rgb {
-  rgb(uint8_t red, uint8_t green, uint8_t blue) : r(red), g(green), b(blue) {};
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
+	rgb(uint16_t red, uint16_t green, uint16_t blue) :
+			r(red), g(green), b(blue) {
+	}
+	uint16_t r;
+	uint16_t g;
+	uint16_t b;
 };
+
+using Magick::Image;
+using Magick::Geometry;
+using Magick::Color;
+using Magick::Quantum;
+using Magick::Drawable;
+using Magick::DrawableText;
+using Magick::DrawableFont;
 
 class background {
 public:
-  background(int width, int height, rgb rgb) :
-    _width(width), _height(height), _rgb(rgb) {}
-  
-  template<typename T> background* merge(T old) {
-    this->_img = gdImageCreateTrueColor(_width, _height);
-    int bg = gdImageColorResolve(_img, _rgb.r, _rgb.g, _rgb.b);
-    gdImageFill(_img, 0, 0, bg);
-    return this;
-  };
-  gdImagePtr img() {
-    return _img;
-  }
+	background(int width, int height, rgb rgb) :
+			_width(width), _height(height), _rgb(rgb) {
+	}
+
+	template<typename T> background* merge(T old) {
+		this->_img = new Image(Geometry(_width, _height),
+				Color(_rgb.r, _rgb.g, _rgb.b, 0));
+		return this;
+	}
+
+	Magick::Image* img() {
+		return _img;
+	}
 private:
-  gdImagePtr _img;
-  int _width;
-  int _height;
-  rgb _rgb;
+	Image* _img;
+	int _width;
+	int _height;
+	rgb _rgb;
 };
 
 class text {
 public:
-  text(const std::string& text, const std::string& font, double size, double angle, int x, int y, rgb rgb) :
-    _text(text), _font(font), _size(size), _angle(angle), _x(x), _y(y), _rgb(rgb) {};
-  template<typename T> text* merge(T old) {
-    this->_img = old->img();
-    int brect[8];
-    int color = gdImageColorResolve(_img, _rgb.r, _rgb.g, _rgb.b);
-    gdImageStringFT(_img, &brect[0], 
-		    color, const_cast<char*>(_font.c_str()), _size, 
-		    _angle, _x, _y, const_cast<char*>(_text.c_str()));
-    return this;
-  };
-  gdImagePtr img() {
-    return _img;
-  }
+	text(const std::string& text, const std::string& font, double size,
+			double angle, int x, int y, rgb rgb) :
+			_text(text), _font(font), _size(size), _angle(angle), _x(x), _y(y), _rgb(
+					rgb) {
+	}
+
+	template<typename T> text* merge(T old) {
+		this->_img = old->img();
+		std::list<Drawable> txt;
+		txt.push_back(DrawableFont("*"));
+		txt.push_back(DrawableText(_x, _y, _text));
+		_img->draw(txt);
+		return this;
+	}
+
+	Image* img() {
+		return _img;
+	}
 private:
-  std::string _text;
-  std::string _font;
-  double _size;
-  double _angle;
-  int _x;
-  int _y;
-  rgb _rgb;
-  gdImagePtr _img;
+	std::string _text;
+	std::string _font;
+	double _size;
+	double _angle;
+	int _x;
+	int _y;
+	rgb _rgb;
+	Image* _img;
 };
 
 class distorting {
 public:
-  template<typename T> distorting* merge(T old) {
-    gdImagePtr ext = gdImageCreateTrueColor(200, 200);
-    gdImageCopy(ext, old->img(), 0, 0, 0, 0, 200, 200);
-    this->_img = gdImageSquareToCircle(ext, 200);
-    return this;
-  };
-  gdImagePtr img() {
-    return _img;
-  };
+	template<typename T> distorting* merge(T old) {
+		this->_img = old->img();
+		return this;
+	}
+
+	Image* img() {
+		return _img;
+	}
+
 private:
-  gdImagePtr _img;
+	Image* _img;
 };
 
 class save {
- public:
- save(const std::string& filename) :
-  _filename(filename) {};
-  template<typename T> save* merge(T old) {
-    FILE* f = fopen(_filename.c_str(), "wb");
-    gdImagePng(old->img(), f);
-    fclose(f);
-    gdImageDestroy(old->img());
-    return NULL;
-  };
- private:
-  std::string _filename;
+public:
+	save(const std::string& filename) :
+			_filename(filename) {
+	}
+
+	template<typename T> save* merge(T old) {
+		Image* img = old->img();
+		img->magick("png");
+		img->write(_filename);
+		return NULL;
+	}
+private:
+	std::string _filename;
 };
