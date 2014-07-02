68f2a54cd58f4960408c9d1493415542692d58b6
Allen Hwang <allenplusplus@gmail.com>
2014-07-02 09:46:33 -0700

    add image loading and compositing utility

--- src/main/java/net/owncaptcha/util/ImageUtil.java
+++ src/main/java/net/owncaptcha/util/ImageUtil.java
@@ -1,9 +1,13 @@
 package net.owncaptcha.util;
 
+import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class ImageUtil {
 	public static final void applyFilter(BufferedImage img, BufferedImageOp filter) {
@@ -20,12 +24,25 @@ public class ImageUtil {
 	}
 	
 	public static final BufferedImage resize(BufferedImage source, int width, int height) {
-	    BufferedImage scaled = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	    BufferedImage scaled = new BufferedImage(width, height, source.getType());
 	    Graphics2D graphics2D = scaled.createGraphics();
 	    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 	    graphics2D.drawImage(source, 0, 0, width, height, null);
 	    graphics2D.dispose();
 	    
 	    return scaled;
+	}
+	
+	public static final BufferedImage load(String filename) throws IOException {
+	    return ImageIO.read(new File(filename));
+	}
+	
+	public static final BufferedImage layer(BufferedImage source, BufferedImage destination, int x, int y) {
+	    Graphics2D graphics = destination.createGraphics();
+	    graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+	    graphics.drawImage(source, null, null);
+	    graphics.dispose();
+	    
+	    return destination;
 	}
 }
